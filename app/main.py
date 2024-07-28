5703a66b01fafdcbb922cddeb3a5c650ee770a8b
Haris Kordić <kordicharis18@gmail.com>
2024-07-28 20:38:15 +0200

    add: displaying results

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, send_from_directory
+from flask import request, render_template, send_from_directory, url_for
 import os
 from app import app
 from utils.image_preprocessing import prepare_image
@@ -23,9 +23,32 @@ def upload_file():
         filename = file.filename
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
+        
+        # Preprocess the image and generate heatmap
         img_array = prepare_image(filepath)
         heatmap = make_gradcam_heatmap(img_array, model, 'conv2d_2', ['flatten', 'dense', 'dense_1'])
         superimposed_img = superimpose_heatmap(filepath, heatmap)
+        
+        # Save the superimposed image
         output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'gradcam_' + filename)
         cv2.imwrite(output_path, superimposed_img)
-        return send_from_directory(app.config['UPLOAD_FOLDER'], 'gradcam_' + filename)
+         # Manually construct the URLs for images
+        original_image_url = f'http://localhost:5000/uploads/{filename}'
+        heatmap_image_url = f'http://localhost:5000/uploads/gradcam_{filename}'
+
+        print(f"Original image URL: {original_image_url}")
+        print(f"Heatmap image URL: {heatmap_image_url}")
+
+        
+        # Return the template with both images
+        return render_template('display_images.html', 
+                               original_image=original_image_url, 
+                               heatmap_image=heatmap_image_url)
+
+@app.route('/uploads/<filename>', methods=['GET'])
+def uploaded_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    print(f"Serving file from: {file_path}")  # Debugging information
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
